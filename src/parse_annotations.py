b34f8e3402bb4f1a2f254ba752727b37080ad227
duolok <dusan.lecic5@gmail.com>
2024-05-20 19:17:33 +0200

    [add] delete starting point

--- src/parse_annotations.py
+++ src/parse_annotations.py
@@ -7,6 +7,3 @@ def parse_annotation(annotation_path):
     filename = root.find('filename').text + ".jpg"
     breed = root.find('object').find('name').text
     return filename, breed
-
-if __name__ == "__main__":
-    print(parse_annotation("../Annotation/n02112137-chow/n02112137_112"))
