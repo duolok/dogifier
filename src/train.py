6be9f8dab183de684c3610f83cd204594663a41b
duolok <dusan.lecic5@gmail.com>
2024-05-22 19:05:39 +0200

    [add] training resnet

---
+++ src/train.py
@@ -0,0 +1,67 @@
+import copy
+import torch
+import torch.optim as optim
+import torch.nn as nn
+from torch.optim import lr_scheduler
+from model import model, device
+from data_processing import dataloaders, dataset_sizes
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print(f'Epoch {epoch}/{num_epochs - 1}')
+        print('-' * 10)
+
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()
+            else:
+                model.eval()
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                optimizer.zero_grad()
+
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            if phase == 'train':
+                scheduler.step()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+    print(f'Best val Acc: {best_acc:4f}')
+
+    model.load_state_dict(best_model_wts)
+    return model
+
+
+model = train_model(model, criterion, optimizer, scheduler, num_epochs=25)
+torch.save(model.state_dict(), 'dog_breed_classifier.pth')
