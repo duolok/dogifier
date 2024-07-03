01c7e3de2b5a67db21c7cc7840d63b2351e5f36f
duolok <dusan.lecic5@gmail.com>
2024-07-03 11:09:46 +0200

    [remove] code cleanup

--- src/parse_annotations.py
+++
@@ -1,9 +0,0 @@
-import xml.etree.ElementTree as ET
-
-def parse_annotation(annotation_path):
-    tree = ET.parse(annotation_path)
-    root = tree.getroot()
-    
-    filename = root.find('filename').text + ".jpg"
-    breed = root.find('object').find('name').text
-    return filename, breed
